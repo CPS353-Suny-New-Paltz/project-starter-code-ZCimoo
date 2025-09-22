9c19dc8dc4631fb35d25a093071463ce6705132a
ZCimoo <zachary.cimorelli09@gmail.com>
2025-09-21 20:47:12 -0400

    Revert "Checkpoint 2 fixes (#19)"
    This reverts commit fb630bb79c0833e33d2e4a3c4f17576d39216293.

--- src/api/DataStorageAPI.java
+++ src/api/DataStorageAPI.java
@@ -4,7 +4,8 @@ import project.annotations.ProcessAPI;
 
 @ProcessAPI
 public interface DataStorageAPI {
-	
+	void processAPI();
+
 	DataReadResponse readData(DataReadRequest dataReadRequest);
 
 	DataWriteResponse writeData(DataWriteRequest dataWriteRequest);
