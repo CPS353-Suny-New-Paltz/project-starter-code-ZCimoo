37de170c46ad11808030fc7f950f009e510d5f54
ZCimoo <zachary.cimorelli09@gmail.com>
2025-09-24 16:43:57 -0400

    DataStorageAPI Implementation
    Completed empty implementation for DataStorageAPI

--- src/implementations/DataStorageImplementation.java
+++ src/implementations/DataStorageImplementation.java
@@ -1,5 +1,21 @@
 package implementations;
 
-public class DataStorageImplementation {
+import api.DataReadRequest;
+import api.DataReadResponse;
+import api.DataStorageAPI;
+import api.DataWriteRequest;
+import api.DataWriteResponse;
+
+public class DataStorageImplementation implements DataStorageAPI {
+	
+	public DataReadResponse readData(DataReadRequest dataReadRequest) {
+		//placeholder return
+		return null;
+	}
+	
+	public DataWriteResponse writeData(DataWriteRequest dataWriteRequest) {
+		//placeholder return
+		return null;
+	}
 
 }
