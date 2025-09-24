75f6b710e573e0fc1ede941b41606b05392e7ef5
ZCimoo <zachary.cimorelli09@gmail.com>
2025-09-24 16:40:32 -0400

    UserNetworkAPI Implementation
    Completed empty implementation for UserNetworkAPI

--- src/implementations/UserNetworkImplementation.java
+++ src/implementations/UserNetworkImplementation.java
@@ -1,5 +1,28 @@
 package implementations;
 
-public class UserNetworkImplementation {
+import api.ComputationInputRequest;
+import api.ComputationInputResponse;
+import api.ComputationOutputRequest;
+import api.ComputationOutputResponse;
+import api.ComputationRequest;
+import api.ComputationResponse;
+import api.UserNetworkAPI;
+
+public class UserNetworkImplementation implements UserNetworkAPI {
+	
+	public ComputationResponse sendRequest(ComputationRequest computationRequest) {
+		//placeholder return
+		return null;
+	}
+	
+	public ComputationInputResponse input(ComputationInputRequest computationInputRequest) {
+		//placeholder return
+		return null;
+	}
+	
+	public ComputationOutputResponse output(ComputationOutputRequest computationOutputRequest) {
+		//placeholder return
+		return null;
+	}
 
 }
