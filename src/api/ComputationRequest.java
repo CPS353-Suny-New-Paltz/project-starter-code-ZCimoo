dd7db31199da432a4b411eec5e38584fbebab740
ZCimoo <zachary.cimorelli09@gmail.com>
2025-10-29 18:04:10 -0400

    Edited Computation Request to suit needs of implementation
    Added input and output path, and updated methods in the class to use them. Have to evaluate the use of the other methods in my implementation.

--- src/api/ComputationRequest.java
+++ src/api/ComputationRequest.java
@@ -5,29 +5,44 @@ import java.util.List;
 public class ComputationRequest {
 	//default delimiter
 	private String delimiter;
-	private String defaultDelimiter = ",";
+	private String inputPath;
+	private String outputPath;
 	
+	//default request, takes input path and delimiter
+	public ComputationRequest(String input, String output, String delimiter) {
+		this.inputPath = input;
+		this.outputPath = output;
+		this.delimiter = delimiter;
+	}
+	
+	//request with only input path and output path, uses default delimiter
+	public ComputationRequest(String input, String output) {
+		this(input, output, ",");
+	}
+	
+	//request with only input path, output path null for now will probably be changed to a default location
+	public ComputationRequest(String input) {
+		this(input, null, ",");
+	}
+	
+	//empty request
 	public ComputationRequest() {
-		this.delimiter = defaultDelimiter;
+		this(null, null, ",");
 	}
 	
-	//custom delimiter
-	public ComputationRequest(String delimiter) {
-		this.delimiter = delimiter;
-	}
-	
-	public ComputationRequest(List<Integer> inputData) {
-		this.delimiter = defaultDelimiter;
-	}
-	
-	public ComputationRequest(List<Integer> inputData, String delimiter) {
-		this.delimiter = delimiter;
-		
-	}
-
 	//get method for delimiter
 	public String getDelimiter() {
 		return delimiter;
+	}
+	
+	//get method for input path
+	public String getInputPath() {
+		return inputPath;
+	}
+	
+	//get method for output path
+	public String getOutputPath() {
+		return outputPath;
 	}
 	
 }
--- src/implementations/UserNetworkImplementation.java
+++ src/implementations/UserNetworkImplementation.java
@@ -6,9 +6,19 @@ import api.ComputationOutputRequest;
 import api.ComputationOutputResponse;
 import api.ComputationRequest;
 import api.ComputationResponse;
+import api.ComputeEngineAPI;
+import api.DataStorageAPI;
 import api.UserNetworkAPI;
 
 public class UserNetworkImplementation implements UserNetworkAPI {
+	
+	private DataStorageAPI dataStore;
+	private ComputeEngineAPI computeEngine;
+	
+	public UserNetworkImplementation(DataStorageAPI ds, ComputeEngineAPI ce) {
+		this.dataStore = ds;
+		this.computeEngine = ce;
+	}
 	
 	@Override
 	public ComputationResponse sendRequest(ComputationRequest computationRequest) {
