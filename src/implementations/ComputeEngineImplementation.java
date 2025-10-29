7291d9fef246ba65a04c7b7d9929c24954ef9b16
ZCimoo <zachary.cimorelli09@gmail.com>
2025-10-29 17:13:44 -0400

    Implement Computation
    Added code to calculate collatz sequence of a given integer.

--- src/implementations/ComputeEngineImplementation.java
+++ src/implementations/ComputeEngineImplementation.java
@@ -1,5 +1,8 @@
 package implementations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import api.ComputationCompleteRequest;
 import api.ComputationCompleteResponse;
 import api.ComputationStartRequest;
@@ -18,5 +21,30 @@ public class ComputeEngineImplementation implements ComputeEngineAPI {
 	public ComputationCompleteResponse completeComputation(ComputationCompleteRequest computationCompleteRequest) {
 		//placeholder return
 		return null;
+	}
+	
+	private List<Integer> computeCollatzSequence(int x){
+		List<Integer> result = new ArrayList<>();
+		
+		//add initial value to sequence
+		result.add(x);
+			
+		//perform calculation until x == 1 (complete computation) 
+		while(x != 1) {
+			//check if x is even
+			if(x % 2 == 0) {
+				//collatz sequence requires even integers to be divided by 2
+				x = x/2;
+				result.add(x);
+			}
+			//check if x is odd
+			else if(x % 2 == 1 ) {
+				//collatz sequence requires odd integers to be multiplied by 3 and add 1
+				x = (x * 3) + 1;
+				result.add(x);
+			}
+		}
+	
+		return result;
 	}
 }
