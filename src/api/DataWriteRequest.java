4e78a685e4265f656fc839b428a0af560c21059f
ZCimoo <zachary.cimorelli09@gmail.com>
2025-11-03 15:10:25 -0500

    Updated DataWriteResponse/Request
    Updated these two before continuing with implementation to better fit needs.

--- src/api/DataWriteRequest.java
+++ src/api/DataWriteRequest.java
@@ -1,19 +1,35 @@
 package api;
 
+import java.util.List;
+
 public class DataWriteRequest {
 
-	private String data;
+	private List<List<Integer>> outputData;
+	private String outputPath;
 	
-	public DataWriteRequest(String data) {
+	public DataWriteRequest(List<List<Integer>> data) {
 		//checks if there is data and assigns it to the data variable
 		if(data == null) {
 		throw new IllegalArgumentException("Data cannot be null");
 	}
-		this.data = data;
+		this.outputData = data;
 }
-	public String getData() {
+	
+	public DataWriteRequest(List<List<Integer>> data, String outputPath) {
+		if(data == null) {
+			throw new IllegalArgumentException("Data cannot be null");
+		}
+			this.outputData = data;
+			this.outputPath = outputPath;
+	}
+	
+	public String getOutputPath() {
+		return this.outputPath;
+	}
+	
+	public List<List<Integer>> getData() {
 		 //returns data
-		return data;
+		return outputData;
 	}
 
 }
