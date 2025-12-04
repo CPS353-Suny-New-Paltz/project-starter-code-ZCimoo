4f3f0efec3344836fb5b659a3c1ef160263e1789
ZCimoo <zachary.cimorelli09@gmail.com>
2025-12-03 21:23:44 -0500

    Updated UserNetwork integeration test
    Also did not pass with new validation checks.

--- test/test/apis/TestUserNetworkAPI.java
+++ test/test/apis/TestUserNetworkAPI.java
@@ -35,16 +35,22 @@ public class TestUserNetworkAPI {
         DataReadResponse mockReadResponse = Mockito.mock(DataReadResponse.class);
         Mockito.when(mockDS.readData(Mockito.any())).thenReturn(mockReadResponse);
         Mockito.when(mockReadResponse.getData()).thenReturn(new ArrayList<Integer>());
+        Mockito.when(mockReadResponse.status()).thenReturn(true);
 
         //Calls write data and returns "true"
         DataWriteResponse mockWriteResponse = Mockito.mock(DataWriteResponse.class);
         Mockito.when(mockDS.writeData(Mockito.any())).thenReturn(mockWriteResponse);
         Mockito.when(mockWriteResponse.status()).thenReturn(true);
+        
+        //Creates mockRequest with values to pass validation
+        ComputationRequest mockRequest = Mockito.mock(ComputationRequest.class);
+        Mockito.when(mockRequest.getInputPath()).thenReturn("dummy-input.txt");
+        Mockito.when(mockRequest.getOutputPath()).thenReturn("dummy-output.txt");
 		
 		
 		//Call sendRequest method with mock request
 		ComputationResponse response = 
-				userNetwork.sendRequest(Mockito.mock(ComputationRequest.class));
+				userNetwork.sendRequest(mockRequest);
 		
 		//Checks if ComputationRequest was successful
 		assertEquals(response.wasSuccessful(), true);
